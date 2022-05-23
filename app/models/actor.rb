bb7ad0bbceb4bba47ea3d57ff64bbd31d50670ad
Samantha Crespo <samkcrespo@gmail.com>
2022-05-23 14:27:00 -0700

    commit tree 7821cd338e9f2bce9fed2019f22b9f05318ad922

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+  "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+  # lists all of the characters that actor has alongside the show that the character is in.
+  # Example -> ['Tyrion Lannister - Game of Thrones']
+  self.characters.map do |character|
+    "#{character.name} - #{character.show.name}"
+  end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+# RETURNS AN ARRAY of the full names of each actor associated with a show. Remember, a show should have many actors through characters. Hint: it may help to use another association macro to access the actors for the show
+self.actors.map do |actor|
+  actor.full_name
+end
+  end
 end
